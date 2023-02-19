4ecc741f02831ca69fa0d2dba8ed68e69f64893a
Jason Dent <jason@streetsidesoftware.nl>
2023-02-19 06:38:05 +0100

    Update processFile.ts

--- src/processFile.ts
+++ src/processFile.ts
@@ -137,6 +137,7 @@ function rebaseRelFileReference(importFile: string, currentFile: string, root: s
         ? relative(currentDir, importFileAbs)
         : relative(targetDir, importFileAbs);
     const newImportFile = normalizeImport(relImport);
+    console.error('%o', { importFile, newImportFile, currentFile, root, target });
     return newImportFile;
 }
 
